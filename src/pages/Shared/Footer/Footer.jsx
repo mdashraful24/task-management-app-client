1accf0481f772abf206338c6979be6d62d8235aa
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-07-10 23:16:32 +0600

    reduce padding

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="p-8 bg-black flex flex-col justify-center gap-1">
+        <div className="p-4 bg-black flex flex-col justify-center gap-1">
             <h2 className='text-xl md:text-3xl font-extrabold text-center text-white'>ProTasker</h2>
             <p className="text-center text-gray-400">&copy; {new Date().getFullYear()} . All rights reserved.</p>
             <p className="text-center text-gray-400">Designed, Developed and Maintained by Ashraful Islam</p>
